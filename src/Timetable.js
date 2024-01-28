8b1f8810b4ec226c7cd0538a0e7f60a1462cb005
sahalkk <msahalkk357@gmail.com>
2024-01-28 14:40:41 +0530

    navbar customized

--- src/Timetable.js
+++ src/Timetable.js
@@ -61,9 +61,17 @@ const Timetable = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      <nav className="navbar bg-gray-900 p-4">
-        <div className="text-white font-bold md:text-xl lg:text-2xl">
-          XYZ MADRASSA
+      <nav className="flex flex-col lg:flex-row items-center navbar bg-gray-900 p-4">
+        <div className="flex border-b-2 lg:w-64 lg:flex-col lg:border-r-2 lg:border-b-0">
+          <div className="text-white font-bold md:text-xl lg:text-2xl">
+            SALSABEEL
+          </div>
+          <div className="text-red-500 font-semibold ms-1 lg:ms-0 md:text-xl lg:text-2xl whitespace-nowrap ">
+            ONLINE MADRASSA
+          </div>
+        </div>
+        <div className="flex items-center ms-4 text-white font-normal text-base md:text-xl">
+          BANGALORE, KARNATAKA
         </div>
       </nav>
       <div className="flex-grow flex justify-center bg-gray-100 md:items-center">
