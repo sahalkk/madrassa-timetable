2e32dcc544a48b1955d747ff94709a7e2d8ce84f
sahalkk <msahalkk357@gmail.com>
2024-02-10 12:10:44 +0530

    ui update

--- src/Timetable.js
+++ src/Timetable.js
@@ -1,5 +1,6 @@
 // Timetable.js
 import React, { useState, useEffect } from "react";
+import html2canvas from "html2canvas";
 
 import fetchData from "./Api";
 
@@ -10,13 +11,28 @@ const Timetable = () => {
   const [selectedValue, setSelectedValue] = useState("");
   const [filteredData, setFilteredData] = useState([]);
 
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   var date = new Date();
 
   useEffect(() => {
     fetchData().then((data) => {
-      let colsArray = data.table.cols.map((item) => item.label).filter(item => (item != "Class " && item != "Grade"));
-      console.log('colsArray ', colsArray);
+      let colsArray = data.table.cols
+        .map((item) => item.label)
+        .filter((item) => item != "Class " && item != "Grade");
+      console.log("colsArray ", colsArray);
       let rowsArray = data.table.rows.map((item, index) => {
         return {
           day: index % 2 == 0 ? item?.c[0]?.v : "",
@@ -63,8 +79,22 @@ const Timetable = () => {
     setFilteredData(filteredDataArray);
   };
 
+  const handleConvertToImage = async () => {
+    const element = document.getElementById("print"),
+      canvas = await html2canvas(element),
+      data = canvas.toDataURL("image/jpg"),
+      link = document.createElement("a");
+
+    link.href = data;
+    link.download = "downloaded-image.jpg";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className="flex flex-col h-screen">
+    <div id="print" className="flex flex-col h-screen">
       <nav className="flex flex-col lg:flex-row items-center navbar bg-sky-950	 p-4">
         <div className="flex border-b-2 lg:w-64 lg:flex-col lg:border-r-2 lg:border-b-0">
           <div className="text-white font-bold md:text-xl lg:text-2xl">
@@ -78,17 +108,19 @@ const Timetable = () => {
           BANGALORE, KARNATAKA
         </div>
       </nav>
-      <div className="flex-grow flex justify-center bg-gray-100 md:items-center time-table-div pt-10">
+
+      <div className="flex-grow flex justify-center bg-gray-100 items-center time-table-div ">
         <div className="flex flex-col  md:text-lg">
           <div className="flex justify-center mt-2 md:mb-6 space-x-4">
-            <label htmlFor="dynamicDropdown">Select Class:</label>
             <select
               id="dynamicDropdown"
               className="rounded bg-white min-w-[100px] border md:min-w-[200px]"
               onChange={handleSelectChange}
               value={selectedValue}
             >
-              <option value="" disabled></option>
+              <option value="" disabled>
+                Select Class
+              </option>
               {classes &&
                 classes.map((option, index) => (
                   <option key={index} value={option}>
@@ -100,7 +132,8 @@ const Timetable = () => {
           {selectedValue ? (
             <>
               <div className="my-1 text-center bg-teal-700 text-slate-50 py-1 text-base rounded-lg	">
-                Class {selectedValue} TimeTable ({months[date.getMonth()] + ' ' + date.getFullYear()})
+                Class {selectedValue} TimeTable (
+                {months[date.getMonth()] + " " + date.getFullYear()})
               </div>
               <table
                 className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
@@ -113,53 +146,65 @@ const Timetable = () => {
                   <tr className="bg-sky-900 text-slate-50	">
                     {cols
                       ? cols.map((item, index) => (
-                        <>
-                          <th
-                            key={index}
-                            scope="col"
-                            className="px-6 py-3"
-                            style={{
-                              border: "1px solid black",
-                              padding: "10px",
-                            }}
-                          >
-                            {item}
-                          </th>
-
-                        </>
-                      ))
+                          <>
+                            <th
+                              key={index}
+                              scope="col"
+                              className="px-6 py-3"
+                              style={{
+                                border: "1px solid black",
+                                padding: "10px",
+                              }}
+                            >
+                              {item}
+                            </th>
+                          </>
+                        ))
                       : null}
                   </tr>
                 </thead>
                 <tbody>
                   {filteredData.map((rowItem, rowIndex) => (
-                    <tr key={rowItem.id} className={`dark:bg-gray-800 dark:border-gray-700 ${((Math.floor(rowIndex / 2)) % 2 == 1) ? '' : 'bg-sky-100'} ${(rowIndex % 2 == 0) ? 'border-t' : ''}`}>
+                    <tr
+                      key={rowItem.id}
+                      className={` dark:border-gray-700 ${
+                        Math.floor(rowIndex / 2) % 2 == 1
+                          ? ""
+                          : "bg-sky-100 text-blue-800"
+                      } ${rowIndex % 2 == 0 ? "border-t" : ""}`}
+                    >
                       {cols
                         ? cols.map((item, index) =>
-                          item != "Grade" && item != "Class" ? (
-                            <td
-                              scope="row"
-                              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                              key={index}
-                              style={{
-                                padding: "7px",
-                                fontSize: "14px",
-                              }}
-                            >
-                              {rowItem[findKey(item)]}
-                            </td>
-                          ) : (
-                            ""
+                            item != "Grade" && item != "Class" ? (
+                              <td
+                                scope="row"
+                                className={
+                                  "px-6 py-4 font-medium text-gray-900 whitespace-nowrap cl-" +
+                                  index
+                                }
+                                key={index}
+                                style={{
+                                  padding: "7px",
+                                  fontSize: "14px",
+                                }}
+                              >
+                                {rowItem[findKey(item)]}
+                              </td>
+                            ) : (
+                              ""
+                            )
                           )
-                        )
                         : null}
                     </tr>
                   ))}
                 </tbody>
               </table>
+              <button type="button" onClick={handleConvertToImage}>
+                Download this table
+              </button>
             </>
           ) : (
-            <div className="mt-4">No Class Selected</div>
+            ""
           )}
         </div>
       </div>
